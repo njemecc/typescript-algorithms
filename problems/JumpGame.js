1619ef81322914ae09c449382ea6b7f1e3379500
brani <branislannjemec@gmail.com>
2024-01-16 21:34:56 +0100

    solve jumpGame

--- problems/JumpGame.js
+++ problems/JumpGame.js
@@ -1,107 +1,112 @@
 // function canJump(nums: number[]): boolean {
+//   if (nums.length === 1) {
+//     console.log("c");
+//     return true;
+//   }
 //   if (nums[0] === 0) {
 //     return false;
 //   }
-//   let jumpPower = nums[0]; //2
-//   let nextIndex = 1; //1
-//   let dummyMax = nums[nextIndex]; //2
+//   let jumpPower = nums[0];
+//   let nextIndex = 1;
+//   let dummyMax = nums[nextIndex];
 //   let dummyIndex = nextIndex;
-//   //permutuje sa sledecim u ovom slucaju 4
-//   do {
+//   let counter = 0;
+//   while (1 == 1) {
 //     if (jumpPower <= 0) {
 //       return false;
 //     }
-//     if (nums.length - 1 - nums.indexOf(jumpPower) <= jumpPower) {
+//     if (nextIndex === nums.length - 1 && nums[nextIndex] > 0 && jumpPower > 0) {
+//       console.log(nums[nextIndex]);
+//       console.log("JAM", jumpPower);
+//       console.log("a");
 //       return true;
 //     }
-//     for (let j = 0; j <= jumpPower; j++) {
-//       if (nums[nextIndex + j] >= dummyMax) {
-//         dummyMax = nums[nextIndex + j];
-//         dummyIndex = nextIndex + j + 1;
-//       }
+//     if (nums.length - 1 - nums.indexOf(jumpPower) <= jumpPower) {
+//       console.log("b");
+//       return true;
 //     }
-//     //update RESET
+//     if (jumpPower === 1) {
+//       nextIndex = nextIndex + 1;
+//       jumpPower = nums[nextIndex - 1];
+//       dummyMax = nums[nextIndex];
+//       dummyIndex = nextIndex;
+//     }
+//     if (jumpPower === 0) {
+//       return false;
+//     }
+//     for (let i = 1; i < jumpPower; i++) {
+//       if (jumpPower === 0) {
+//         return false;
+//       }
+//       if (jumpPower === 1) {
+//         console.log("ee", jumpPower);
+//         nextIndex = nextIndex + 1;
+//         jumpPower = nums[nextIndex - 1];
+//         dummyMax = nums[nextIndex];
+//         dummyIndex = nextIndex;
+//       }
+//       if (jumpPower > 1) {
+//         if (nums[nextIndex + i] >= dummyMax) {
+//           console.log("usao u if");
+//           counter++;
+//           dummyMax = nums[nextIndex + i];
+//           dummyIndex = nextIndex + i + 1;
+//         } else if (counter === 0) {
+//           nextIndex = nextIndex + 1;
+//           jumpPower = nums[nextIndex - 1];
+//           dummyMax = nums[nextIndex];
+//           dummyIndex = nextIndex;
+//           //resetujemo i
+//           i = 1;
+//         }
+//       }
+//       console.log("1", nums[nextIndex + 1]);
+//       console.log("dummyMax", dummyMax);
+//     }
 //     jumpPower = dummyMax;
 //     nextIndex = dummyIndex;
 //     console.log("JP", jumpPower);
 //     console.log("NI", nextIndex);
-//   } while (nextIndex != nums.length - 1);
+//   }
+//   //nepotrebno
 //   return true;
 // }
 function canJump(nums) {
+    var jumpPower = nums[0];
+    var currIndex = 0;
     if (nums.length === 1) {
-        console.log("c");
         return true;
     }
-    if (nums[0] === 0) {
-        return false;
-    }
-    var jumpPower = nums[0];
-    var nextIndex = 1;
-    var dummyMax = nums[nextIndex];
-    var dummyIndex = nextIndex;
-    var counter = 0;
-    while (1 == 1) {
-        if (jumpPower <= 0) {
-            return false;
-        }
-        if (nextIndex === nums.length - 1 && nums[nextIndex] > 0 && jumpPower > 0) {
-            console.log(nums[nextIndex]);
-            console.log("JAM", jumpPower);
-            console.log("a");
-            return true;
-        }
-        if (nums.length - 1 - nums.indexOf(jumpPower) <= jumpPower) {
-            console.log("b");
-            return true;
-        }
-        if (jumpPower === 1) {
-            nextIndex = nextIndex + 1;
-            jumpPower = nums[nextIndex - 1];
-            dummyMax = nums[nextIndex];
-            dummyIndex = nextIndex;
-        }
-        if (jumpPower === 0) {
-            return false;
-        }
-        for (var i = 1; i < jumpPower; i++) {
-            if (jumpPower === 0) {
-                return false;
-            }
-            if (jumpPower === 1) {
-                console.log("ee", jumpPower);
-                nextIndex = nextIndex + 1;
-                jumpPower = nums[nextIndex - 1];
-                dummyMax = nums[nextIndex];
-                dummyIndex = nextIndex;
-            }
-            if (jumpPower > 1) {
-                if (nums[nextIndex + i] >= dummyMax) {
-                    console.log("usao u if");
-                    counter++;
-                    dummyMax = nums[nextIndex + i];
-                    dummyIndex = nextIndex + i + 1;
-                }
-                else if (counter === 0) {
-                    nextIndex = nextIndex + 1;
-                    jumpPower = nums[nextIndex - 1];
-                    dummyMax = nums[nextIndex - 1];
-                    dummyIndex = nextIndex;
-                    //resetujemo i
-                    i = 1;
-                }
-            }
-            console.log("1", nums[nextIndex + 1]);
-            console.log("dummyMax", dummyMax);
-        }
-        jumpPower = dummyMax;
-        nextIndex = dummyIndex;
+    while (true) {
         console.log("JP", jumpPower);
-        console.log("NI", nextIndex);
+        console.log("CI", currIndex);
+        if (jumpPower === 1) {
+            currIndex = currIndex + 1;
+            jumpPower = nums[currIndex];
+        }
+        if (jumpPower >= nums.length - 1 - currIndex) {
+            return true;
+        }
+        if (jumpPower === 0 && currIndex !== nums.length - 1) {
+            return false;
+        }
+        if (currIndex === nums.length - 1) {
+            return true;
+        }
+        var i = 1;
+        var nextIndex = currIndex + 1;
+        for (i; i <= jumpPower; i++) {
+            if (nums[currIndex + i] >= nums[nextIndex]) {
+                console.log("nextIndex", nextIndex);
+                console.log("curr index + ".concat(i), currIndex + i);
+                nextIndex = currIndex + i;
+            }
+        }
+        currIndex = nextIndex;
+        jumpPower = nums[currIndex];
+        if (currIndex === nums.length - 1) {
+            return true;
+        }
     }
-    //nepotrebno
-    return true;
 }
-//JM = 1
-//NI = 2
-console.log(canJump([1, 2, 3]));
+console.log(canJump([3, 0, 8, 2, 0, 0, 1]));
