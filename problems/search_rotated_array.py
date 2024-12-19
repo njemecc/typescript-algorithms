359d26c16fac70cf3726345a51fb94093f7a021e
Branislav Njemec <105017037+njemecc@users.noreply.github.com>
2024-12-19 09:47:50 +0100

    Update search_rotated_array.py

--- problems/search_rotated_array.py
+++ problems/search_rotated_array.py
@@ -7,28 +7,37 @@ class Solution(object):
             mid = (l + r) // 2
             print("MIDValue:",nums[mid])
 
-            if nums[mid] == target:
+            if target == nums[mid]:
                 return mid
-            if target > nums[mid]:
+            elif target < nums[mid]:
+                #na desnoj strani je mid
+                if nums[mid] < nums[l]:
+                    r = mid - 1
+                #na levoj strani je mid
+                else:
+                    if target >= nums[l]:
+                        r = mid -1
+                    else:
+                        l = mid + 1
+            elif target > nums[mid]:
+                # na levoj smo strani
                 if nums[mid] >= nums[l]:
                     l = mid + 1
-                else:
-                    if target > nums[r]:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
-            elif target < nums[mid]:
-                if nums[mid] >= nums[l]:
-                    if target >= nums[l]:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
+                # na desnoj smo strani i target = veci od mid-a
                 else:
                     if target > nums[r]:
                         r = mid - 1
                     else:
                         l = mid + 1
 
-        return -1
 
-print(Solution.search("this",[5,1,2,3,4],1))
+
+
+
+
+
+
+        return  -1
+
+
+print(Solution.search("this",[5,1,2,3,4],1));
